5333b32d8ca2e5082eac8424ef84f27b4cf931e9
Acen <acenqiu@gmail.com>
2018-03-21 17:30:22 +0800

    add zh-CN locale

---
+++ locales/zh-CN.js
@@ -0,0 +1,11 @@
+const metadata = {
+  description: '待办事项插件',
+  actionProviders: [
+    {
+      name: "转换成Todo",
+      description: "将这段文字转换成一个Todo项"
+    }
+  ]
+}
+
+module.exports = {metadata}
