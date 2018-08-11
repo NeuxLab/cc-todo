9fab01578f1140a8b42177c15585e35ae6fd2c6d
gockxml <hustgock@gmail.com>
2018-08-11 22:34:55 +0800

    better tab guide

--- lib/components/tab-guide.vue
+++ lib/components/tab-guide.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    提示：cc后显示通知时，可以按tab键进行处理，比如添加一条待办事项。
+  <div class='tab-guide-todo'>
+    <div class='caption'></div>
+   <p>提示：cc后显示通知时，可以按tab键进行处理，比如添加一条待办事项。</p>
   </div>
 </template>
